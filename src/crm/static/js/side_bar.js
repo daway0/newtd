0ce2f14496765bda8c75dc4a176ef6971fe6cfaf
Kimia <rad.kimia14@gmail.com>
2024-05-04 22:11:11 +0330

    The state of the menu is saved on the client side from now on

--- src/crm/static/js/side_bar.js
+++ src/crm/static/js/side_bar.js
@@ -1,11 +1,30 @@
 $(document).ready( function () {
+
+    var menuState = localStorage.getItem("menuState")
+
+    if (menuState == "open" || menuState == null){
+        $("#sidemenu-minimized").addClass("hidden")
+        $("#sidemenu").removeClass("hidden")
+    }
+    if(menuState == "closed"){
+        $("#sidemenu").addClass("hidden")
+        $("#sidemenu-minimized").removeClass("hidden")
+    }
+
     $(document).on('click', '#sidemenu, #sidemenu-minimized', function () {
         $("#sidemenu").toggleClass("hidden")
         $("#sidemenu-minimized").toggleClass("hidden")
+
+        if ($("#sidemenu").hasClass("hidden")) {
+            localStorage.setItem("menuState",  "closed");  
+        }else{
+            localStorage.setItem("menuState", "open");
+        }
         
     });
     $(document).on('click', '.menu-item', function () {
         $("#sidemenu").toggleClass("hidden")
         $("#sidemenu-minimized").toggleClass("hidden")
     });
+    
 })
