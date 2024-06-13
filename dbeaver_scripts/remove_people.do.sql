f2414c2e7464d688174c4c6aea132813dafe54d2
daway0 <rezaee.erfan.2000@gmail.com>
2024-06-14 02:11:31 +0330

    dynamic query

--- dbeaver_scripts/remove_people.do.sql
+++ dbeaver_scripts/remove_people.do.sql
@@ -1,9 +1,17 @@
 do
 $$
     declare
-        v_people_id            crm_people.id%TYPE;
-        v_people_full_name     varchar;
-        v_people_national_code crm_people.national_code%TYPE = '0023403618';
+        v_people_id                      crm_people.id%TYPE;
+        v_people_full_name               varchar;
+        v_people_national_code           crm_people.national_code%TYPE = '0023403616';
+        v_tables_have_people_id constant varchar[]=array [
+            'crm_specification',
+            'crm_servicelocation',
+            'crm_peopletype',
+            'crm_peoplerole',
+            'crm_peopledetailedinfo'];
+        v_table_have_people_id           varchar;
+        v_dynamic_query                  text;
     begin
         select id
         into v_people_id
@@ -22,35 +30,16 @@ $$
 
         raise notice 'The People Was: %', v_people_full_name;
 
-        delete
-        from crm_specification
-        where people_id = v_people_id;
-        raise notice 'crm_specification deleted';
-
-        delete
-        from crm_servicelocation
-        where people_id = v_people_id;
-        raise notice 'crm_servicelocation deleted';
-
-        delete
-        from crm_peopletype
-        where people_id = v_people_id;
-        raise notice 'crm_peopletype deleted';
-
-        delete
-        from crm_peoplerole
-        where people_id = v_people_id;
-        raise notice 'crm_peoplerole deleted';
-
-        delete
-        from crm_peopledetailedinfo
-        where people_id = v_people_id;
-        raise notice 'crm_peopledetailedinfo deleted';
+        foreach v_table_have_people_id in array v_tables_have_people_id
+            loop
+                v_dynamic_query = 'delete from ' || v_table_have_people_id || ' where people_id =' || v_people_id;
+                execute v_dynamic_query;
+                raise notice '% table data related deleted', v_table_have_people_id;
+            end loop;
 
         delete
         from crm_people
         where id = v_people_id;
         raise notice 'people deleted';
-
     end;
 $$
